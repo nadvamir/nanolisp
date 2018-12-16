74ba06829b49407f2aff6fc977ac30967cdb7623
Maksim Solovjov <himaxx2@gmail.com>
2018-12-16 20:43:14 +0000

    Provide repl backbone

--- nanolisp.py
+++ nanolisp.py
@@ -3,5 +3,14 @@
 def get_welcome_text():
     return "NanoLisp v0.1 REPL"
 
+def evaluate():
+    return False
+
+def repl():
+    command = raw_input("> ")
+    return evaluate(command)
+
 if __name__ == "__main__":
     print(get_welcome_text())
+    while repl():
+        pass
