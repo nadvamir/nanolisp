729b554c56b1d5097c4e3d6bd227d3a64132d320
Maksim Solovjov <himaxx2@gmail.com>
2018-12-16 20:46:11 +0000

    Cheat. Should have reverted the previous diff, but didn't test. Fix issues

--- nanolisp.py
+++ nanolisp.py
@@ -3,11 +3,11 @@
 def get_welcome_text():
     return "NanoLisp v0.1 REPL"
 
-def evaluate():
+def evaluate(command):
     return False
 
 def repl():
-    command = raw_input("> ")
+    command = input("> ")
     return evaluate(command)
 
 if __name__ == "__main__":
